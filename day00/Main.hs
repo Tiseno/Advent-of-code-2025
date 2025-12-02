2eb84ccafa3784faf9559ff7a33f861f727f3242
Tiseno <math.lindgren@gmail.com>
2025-12-02 08:40:11 +0100

    Added day template and readme.

---
+++ day00/Main.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+import qualified Data.List.Split as Split
+
+parseInput :: String -> _
+parseInput input = lines $ input
+
+part1 :: _ -> _
+part1 input = input
+
+
+main :: IO ()
+main = do
+  input <- parseInput <$> readFile "input.0.txt"
+  putStr "Part 1 : "
+  print $ part1 input
