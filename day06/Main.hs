3113b8251dd1aceb185188ddcac68072bf1d48f5
Tiseno <math.lindgren@gmail.com>
2025-12-06 09:29:58 +0100

    Day 6 part 2.

--- day06/Main.hs
+++ day06/Main.hs
@@ -1,21 +1,35 @@
-import           Data.List (transpose)
+import qualified Data.Char       as Char
+import           Data.List       as List
+import qualified Data.List.Split as Split
 
 type Operator = Int -> Int -> Int
 type Problem = (Operator, [Int])
-parseInput :: String -> _
-parseInput input = parseProblem . reverse <$> transpose (words <$> lines input)
+
+parseOperator :: String -> Operator
+parseOperator "*" = (*)
+parseOperator "+" = (+)
+
+solve :: (String -> [Problem]) -> String -> Int
+solve parseProblems input = sum (uncurry foldr1 <$> parseProblems input)
+
+parse1 :: String -> [Problem]
+parse1 input = parseProblem . reverse <$> List.transpose (words <$> lines input)
   where
     parseProblem :: [String] -> Problem
-    parseProblem (x:xs) = (parseOperator x, fmap read xs)
-    parseOperator :: String -> Operator
-    parseOperator "*" = (*)
-    parseOperator "+" = (+)
+    parseProblem (x:xs) = (parseOperator x, read <$> xs)
 
-part1 :: [Problem] -> Int
-part1 input = sum $ fmap (uncurry foldr1) input
+parse2 :: String -> [Problem]
+parse2 input = fmap parseProblem $ Split.splitWhen (all (== ' ')) $ List.transpose $ lines input
+  where
+    parseProblem :: [String] -> Problem
+    parseProblem (x:xs) =
+      let (num, op) = span (\c -> Char.isDigit c || Char.isSpace c) x
+       in (parseOperator op, read <$> (num:xs))
 
 main :: IO ()
 main = do
-  input <- parseInput <$> readFile "input.1.txt"
+  input <- readFile "input.1.txt"
   putStr "Part 1 : "
-  print $ part1 input
+  print $ solve parse1 input
+  putStr "Part 2 : "
+  print $ solve parse2 input
